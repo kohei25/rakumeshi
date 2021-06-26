f98485244cd5a99c15ca56fa360f9c025b2d5936
ykymst <yukiyama913@gmail.com>
2021-06-26 20:08:03 +0900

    get all restaurant data

---
+++ data/data_preparation.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sqlite3
+from get_restaurant_data import get_dataframe
+
+df = get_dataframe()
+
+# Save dataframe as sqlite3
+
+# preprocess dataframe
+
+# calculate similarity among restaurants and save it as sqlite3
