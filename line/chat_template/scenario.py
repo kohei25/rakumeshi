f270a4e857d17208ecb48b08de31cff1f51db741
kohei25 <nicodoteng@gmail.com>
2021-07-03 16:37:09 +0900

    add line user db

--- line/chat_template/scenario.py
+++ line/chat_template/scenario.py
@@ -1,16 +1,29 @@
 from linebot.models import (
 	TextMessage
 )
-from chat_template.message_tpl import (
+from .message_tpl import (
 	carousel_columns, carousel_template_message, confirm_tpl
 )
-from chat_template.test_data import (
+from .test_data import (
 	test_data
 )
+
+from line.models import User
+from line.application import db
+
+def add_user(line_id):
+	user = User.query.filter_by(line_id=line_id)
+	if user is None:
+		n_user = User(line_id)
+		db.session.add(n_user)
+		db.session.commit()
+	return
 
 def chat_scenario(line_bot_api, event, text):
 	if text == 'ラクメシ' or text == '最初から選択しなおす':
 		c_tpl = confirm_tpl()
+		line_id = event.source.user_id
+		add_user(line_id)
 		line_bot_api.reply_message(
 			event.reply_token,
 			c_tpl
