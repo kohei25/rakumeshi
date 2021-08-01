079b9d895a7e5a8e88ccce0fe87359579364d933
kohei25 <koheeei25@gmail.com>
2021-08-01 21:18:08 +0900

    add schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,26 +1,24 @@
-CREATE TABLE restraunts (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    restraunts_name TEXT NOT NULL
-);
 CREATE TABLE users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     line_id TEXT NOT NULL UNIQUE,
     created_at INTEGER
 );
-CREATE TABLE uer_features (
+CREATE TABLE user_features (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
     sex INTEGER,
     genre INTEGER,
     budget INTEGER,
     created_at INTEGER NOT NULL,
-    update_at INTEGER NOT NULL,
+    updated_at INTEGER NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id)
 );
 CREATE TABLE shop_evaluation(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
+    restraunt_id INTEGER,
     evaluation INTEGER,
     created_at INTEGER NOT NULL,
-    FOREIGN KEY(user_id) REFERENCES users(id)
+    FOREIGN KEY(user_id) REFERENCES users(id),
+    FOREIGN KEY(restraunt_id) REFERENCES restraunts(id)
 );
