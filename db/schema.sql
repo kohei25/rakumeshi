880cf413ac68ec06d9b7fc996fb274c1b753ebf9
kohei25 <koheeei25@gmail.com>
2021-08-13 17:17:51 +0900

    modify scheme.sql

--- db/schema.sql
+++ db/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE shop_evaluation(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
     restraunt_id INTEGER,
-    evaluation INTEGER,
+    rating INTEGER,
     created_at INTEGER NOT NULL,
     FOREIGN KEY(user_id) REFERENCES users(id),
     FOREIGN KEY(restraunt_id) REFERENCES restraunts(id)
