dcb0d78cccb813dbe7597ea53b6b94b2eb0eba7d
tsuguro529 <tsuguro4649@gmail.com>
2021-09-08 16:48:46 -0700

    #33 update reprocess user features

--- db/preprocess_user_data.py
+++ db/preprocess_user_data.py
@@ -1,21 +1,39 @@
 import pandas as pd
 import numpy as np
 
-from database import db_session
-from models import User, UserFeatures, ShopEvaluation
+import sqlite3
 
-def get_data():
-    #get features data from SQL
-    Base = declarative_base()
-    Base.query = db_session.query_property()
-    UF = UserFeatures
-    data_user = db_session.query(UF.user_id, UF.sex, UF.genre, UF.budget).all()
+def get_latest_data(index):
+    #get features latest data from SQL
+    con = sqlite3.connect('../rakumeshi.db')
+    cur = con.cursor()
+    query = f'SELECT age, budget, sex, genre FROM user_features'
+    data = cur.execute(query).fetchall()
+    con.commit()
+    return data[index]
 
-    return data_user
 
 def get_binary_vector(data):
     #convert data into binary vector through pandas dataframe
-    dfm = pd.DataFrame(data)
-    vector = pd.get_dummies(dfm).values
-    
-    return vector
+    dfm = pd.DataFrame(data).T.set_axis(['age', 'budget', 'sex', 'genre'], axis=1)
+    sex_categories = {0, 1, 2}
+    genre_categories = {1, 2, 3, 4, 5, 6, 7}
+    dfm['sex'] = pd.Categorical(dfm['sex'], categories=sex_categories)
+    dfm['genre'] = pd.Categorical(dfm['genre'], categories=genre_categories)
+    binary_vector = pd.get_dummies(dfm, columns=['sex', 'genre']).values
+    return binary_vector
+
+
+def write_binary_vector(binary_vector):
+    #read numpy data and append binary vector as numpy file
+    numpy_data = np.load('user_features.npy')
+    update_numpy_data = np.append(numpy_data, binary_vector, axis=0)
+    np.save('user_features.npy', update_numpy_data)
+    return
+
+
+if __name__ == '__main__':
+    #np.save('user_features.npy', np.empty((0,12)))  #create empty numpy data
+    latest_data = get_latest_data(-1)
+    binary_vector = get_binary_vector(latest_data)
+    write_binary_vector(binary_vector)
