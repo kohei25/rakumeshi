6916d2a1925c4a9fbc0fb24c84167a557764602a
kohei25 <koheeei25@gmail.com>
2021-09-15 19:58:12 +0900

    add files for production

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask',
+        'Flask-SQLAlchemy',
+        'line-bot-sdk',
+        'python-dotenv',
+        'psycopg2-binary'
+    ],
+)
