6bf8d48a42ba28d6d4f0ad0816d845f27b477b7e
kohei25 <koheeei25@gmail.com>
2021-09-19 20:20:36 +0900

    split function in line scenario

--- flaskr/linebot/chat/scenario.py
+++ flaskr/linebot/chat/scenario.py
@@ -9,38 +9,29 @@ from .message_template import (
 from .message_content import (
     first_rep
 )
-from linebot.models.actions import (
-    PostbackAction
-)
 
-# from .test_data import (
-#     test_data
-# )
+
+def call_me(line_bot_api, event):
+    first_rep(line_bot_api, event)
+    return
 
 def chat_scenario(line_bot_api, event):
-    e_type = event.type
-    print(event)
-    if e_type == 'message':
-        text = event.message.text
-        if text == 'ラクメシ':
-            first_rep(line_bot_api, event)
-    elif e_type == 'postback':
-        postback = event.postback.data
-        if postback == 'location_search':
-            line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(
-                    text=''
-                )
+    postback = event.postback.data
+    if postback == 'location_search':
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(
+                text=''
             )
-            return
-        elif 'lat' in postback:
-            name = re.findall('name=(.*)&a',postback)[0]
-            address = re.findall('address=(.*)&lat', postback)[0]
-            lat = re.findall('lat=(.*)&', postback)[0]
-            lng = re.findall('lng=(.*)', postback)[0]
-            line_bot_api.reply_message(
-                event.reply_token,
-                loc_tpl(name, address, float(lat), float(lng))
-            )
+        )
+        return
+    elif 'lat' in postback:
+        name = re.findall('name=(.*)&a',postback)[0]
+        address = re.findall('address=(.*)&lat', postback)[0]
+        lat = re.findall('lat=(.*)&', postback)[0]
+        lng = re.findall('lng=(.*)', postback)[0]
+        line_bot_api.reply_message(
+            event.reply_token,
+            loc_tpl(name, address, float(lat), float(lng))
+        )
     return
--- flaskr/linebot/line.py
+++ flaskr/linebot/line.py
@@ -5,8 +5,12 @@ from linebot.exceptions import (
 from linebot import (
 	LineBotApi, WebhookHandler
 )
-from .chat.scenario import chat_scenario
+from .chat.scenario import (
+    chat_scenario, call_me
+)
+from .models import User
 from flaskr.config import config
+from flaskr import db
 
 class Line(object):
     def __init__(self, app):
@@ -26,7 +30,29 @@ class Line(object):
         return
 
     def scenario(self, event):
-        chat_scenario(self.line_bot_api, event)
+        print(event)
+
+        if event.message.text =="ラクメシ":
+            e_source = event.source
+            e_source_type = e_source.type
+            # todo : e_source_typeがgroupとuserでレスポンスをかえる
+            call_me(self.line_bot_api, event)
+        elif event.type == 'postback':
+            chat_scenario(self.line_bot_api, event)
         return
+
+    def get_group_users(self, group_id):
+        '''
+        only use for official account and premium account
+        so you use unofficial account, you don't use this function
+        '''
+        member_ids = self.line_bot_api.get_group_member_ids(group_id)
+        return member_ids
     
-    
+    def add_user(self, lineid):
+        user = User.query.filter_by(lineid=lineid).first()
+        print('add user')
+        if user is None:
+            db.session.add(User(lineid=lineid))
+            db.session.commit()
+        return
