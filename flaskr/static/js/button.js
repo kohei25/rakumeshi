5673bc1e5c925de031052b99afa578d23ccf9305
kohei25 <koheeei25@gmail.com>
2021-09-19 20:10:32 +0900

    delete waste files

--- flaskr/static/js/button.js
+++
@@ -1,7 +0,0 @@
-window.onload = function(){
-    const button = document.getElementById('openWindowButton')
-
-    button.addEventListener('click', event => {
-        button.textContent = `Click count: ${event.detail}`
-    });
-}
