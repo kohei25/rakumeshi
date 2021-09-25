6a8037151d8f0c773bd06d1c58f5e435c2e0b4bc
kohei25 <koheeei25@gmail.com>
2021-09-26 07:03:20 +0900

    change line scenario for user

--- flaskr/linebot/line.py
+++ flaskr/linebot/line.py
@@ -1,3 +1,8 @@
+from datetime import datetime
+import numbers
+import re
+import time
+from flaskr.api.models import UserFeature
 from flask import request, abort
 from linebot.exceptions import (
 	InvalidSignatureError
@@ -6,9 +11,9 @@ from linebot import (
 	LineBotApi, WebhookHandler
 )
 from .chat.scenario import (
-    chat_scenario, call_me
+    chat_for_group, chat_for_user_registrated, chat_for_user_unregistrated, chat_scenario, call_me, request_rating_restaurant, send_invitation, send_reccomendated_restaurants
 )
-from .models import User
+from .models import RatingRestaurant, SelectedRestaurant, User
 from flaskr.config import config
 from flaskr import db
 
@@ -30,13 +35,65 @@ class Line(object):
         return
 
     def scenario(self, event):
-        if event.message.text =="ラクメシ":
-            e_source = event.source
-            e_source_type = e_source.type
-            # todo : e_source_typeがgroupとuserでレスポンスをかえる
-            call_me(self.line_bot_api, event)
-        elif event.type == 'postback':
-            chat_scenario(self.line_bot_api, event)
+        '''
+        - userの状況（好み登録済みかどうか）
+        - 呼び出し場所（user or group）
+        によってシナリオを変える
+        '''
+        print(event)
+        e_source = event.source
+        e_type = event.type
+        if e_type == 'message':
+            if event.message.text == "ラクメシ":
+                e_source_type = e_source.type
+                line_id = e_source.user_id
+                print('e_source_type:', e_source_type, 'user_id:', line_id)
+                # todo : e_source_typeがgroupとuserでレスポンスをかえる
+                if e_source_type == 'group':
+                    group_id = e_source.group_id
+                    count = self.get_group_count(group_id)
+                    chat_for_group(self.line_bot_api, event,  count, group_id)
+                elif e_source_type == 'user':
+                    checked = self.check_registrated_preference(line_id)
+                    print(checked)
+                    if checked:
+                        # 好みを登録済み
+                        chat_for_user_registrated(self.line_bot_api, event)
+                    else:
+                        # 好みを未登録
+                        chat_for_user_unregistrated(self.line_bot_api, event)
+            # FIXME: トリガーを変更する
+            elif event.message.text == "評価":
+                data = {
+                    
+                }
+                request_rating_restaurant(self.line_bot_api, event.source.user_id, data)
+        elif e_type == 'postback':
+            e_postback = event.postback
+            data = e_postback.data
+            m = re.match(r'action=([a-z]+)&([a-zA-Z0-9=]+)', data).groups()
+            action = m[0]
+            if action == 'rating':
+                user = self.check_user(e_source.user_id)
+                rate = re.match(r'rate=([0-9])', m[1]).groups()[0]
+                print(f'rate: {rate}')
+                created_at = datetime.now()
+                # FIXME: restaurant idのデータを取得する
+                db.session.add(RatingRestaurant(user_id= user.id, restaurant_id=1 , rate=rate, created_at=created_at))
+                db.session.commit()
+                # TODO: update selectedRestaurant status
+                # TODO: rating thank you message
+        return
+
+    def push_message(self, to, data):
+        '''
+        to: userId, groupId
+        status | 0: event create -> send invitaton, 1: close event -> recommend restaurant
+        '''
+        if data['status'] == 0:
+            send_invitation(self.line_bot_api, to, data['invitation'])
+        elif data['status'] == 1:
+            send_reccomendated_restaurants(self.line_bot_api, to)
         return
 
     def get_group_users(self, group_id):
@@ -46,11 +103,38 @@ class Line(object):
         '''
         member_ids = self.line_bot_api.get_group_member_ids(group_id)
         return member_ids
+
+    def get_group_count(self, group_id):
+        count = self.line_bot_api.get_group_members_count(group_id)
+        return count
     
-    def add_user(self, lineid):
-        user = User.query.filter_by(lineid=lineid).first()
-        print('add user')
+    def check_user(self, line_id):
+        user = User.query.filter_by(line_id=line_id).first()
         if user is None:
-            db.session.add(User(lineid=lineid))
+            db.session.add(User(line_id=line_id))
             db.session.commit()
+            user = User.query.filter_by(line_id=line_id).first()
+        return user
+    
+    def check_registrated_preference(self, line_id):
+        status = False
+        user = User.query.filter_by(line_id=line_id).first()
+        if user is None:
+            return status
+        checked = UserFeature.query.filter_by(user_id=user.id)
+        if checked is not None:
+            status = True
+        return status
+    
+    def monitor_db(self):
+        while(True):
+            # FIXME: arrange interval time production 60*60 (s)
+            interval = 10
+            time.sleep(interval)
+            request_rating_users = SelectedRestaurant.query.filter_by(status=0).all()
+            for user in request_rating_users:
+                request_rating_restaurant(self.line_bot_api, user.line_id, )
+
+            # TODO: push request rating message
+            print('hi')
         return
